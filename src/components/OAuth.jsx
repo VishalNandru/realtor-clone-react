af7203261ce68cb4458641f0b72922e6370382e7
Vishal Nandru <vishalnandru@Vishals-MacBook-Air.local>
2023-06-27 13:22:58 +0530

    complete the oauth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,44 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import React from 'react'
 import {FcGoogle} from "react-icons/fc"
+import { toast } from 'react-toastify'
+import { db } from '../firebase';
+import { useNavigate } from 'react-router';
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth,provider);
+      const user = result.user;
+
+      // checking if the user is already present in db
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef) ;
+
+      if (!docSnap.exists()){
+        await setDoc(docRef, {
+          name:user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        })
+      }
+      navigate("/");
+      
+      
+    } catch (error) {
+      toast.error("Could not authorize with google");
+      
+    }
+
+  }
+
   return (
-    <button className='flex items-center justify-center w-full
+    <button type = "button" onClick={onGoogleClick} className='flex items-center justify-center w-full
      bg-red-700 text-white px-7 py-3 
      uppercase rounded text-sm font-medium
       hover:bg-red-800 active:bg-red-900 
