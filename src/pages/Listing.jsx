4c8392266fdbcfbec37d483effb1d17e19c1b4ba
VishalNandru <vishalnandru4@gmail.com>
2023-07-23 20:31:21 -0400

    create slider component and add it to the home page.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -160,13 +160,15 @@ export default function Listing() {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className=" w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6
-        md:mt-0 md:ml-2">
+        <div
+          className=" w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6
+        md:mt-0 md:ml-2"
+        >
           <MapContainer
             center={[listing.geolocation.lat, listing.geolocation.lng]}
             zoom={13}
             scrollWheelZoom={false}
-            style={{height:"100%", width:"100%"}}
+            style={{ height: "100%", width: "100%" }}
           >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -175,9 +177,7 @@ export default function Listing() {
             <Marker
               position={[listing.geolocation.lat, listing.geolocation.lng]}
             >
-              <Popup>
-                {listing.address}
-              </Popup>
+              <Popup>{listing.address}</Popup>
             </Marker>
           </MapContainer>
         </div>
